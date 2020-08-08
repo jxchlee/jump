bfd9fb8424e43b8b6d2fa6aee47a8fd2187431e0
eunsagn <dmstkdrmatkd@naver.com>
2020-08-08 01:28:19 +0000

    re-add fac2

--- main.cpp
+++ main.cpp
@@ -15,20 +15,30 @@ long long fac1(int n1, int n2){
   swap(n1, n2);
   }
   if(n1 == 0) return 1;
-  return (n2+n1) / n1 * fac1(n1 - 1, n2);
+  printf("%d %d\n", n1, n2);
+  return (n2+n1) * fac1(n1 - 1, n2);
 }
+
+long long fac2(int n1, int n2){
+  if(n1>n2){
+  swap(n1, n2);
+  }
+  if(n1 == 0) return 1;
+  return n1 * fac2(n1 - 1, n2);
+}
+
 
 long long solution(int n) {
   long long answer = 0;
-  int one =n, two;
-  int tem=0;
+  int one, two;
+  long long tem=0;
 
   for(two=0; 2*two<=n; two++){
     one = n - 2 * two;
     printf("%d %d\n", one, two);
-    tem = fac1(one, two) % 1234567;
+    tem = (fac1(one, two) / fac2(one, two)) % 1234567;
     answer += tem;
-    printf("%d\n", tem);
+    printf("%lld\n", tem);
   }
 
   answer %= 1234567;
@@ -38,7 +48,7 @@ long long solution(int n) {
 
 int main(){
 
-  int n = 4;
+  int n = 10;
   solution(n);
   return 0;
 }
