b35231e181a6c6b3c099156cb8824d80bff1d37a
eunsagn <dmstkdrmatkd@naver.com>
2020-08-08 01:29:32 +0000

    test fac2

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void swap(int  &n1, int  &n2){
   n2 = z;
   
 }
-
+/*
 long long fac1(int n1, int n2){
   if(n1>n2){
   swap(n1, n2);
@@ -18,19 +18,28 @@ long long fac1(int n1, int n2){
   printf("%d %d\n", n1, n2);
   return (n2+n1) * fac1(n1 - 1, n2);
 }
-
+*/
+long long fac2(int n1, int n2){
+  if(n1>n2){
+  swap(n1, n2);
+  }
+  if(n1 == 0) return 1;
+  printf("%d %d", n1, n2);
+  return n1 * fac2(n1 - 1, n2);
+}
 
 /*
 long long solution(int n) {
   long long answer = 0;
-  int one =n, two;
-  int tem=0;
+  int one, two;
+  long long tem=0;
 
   for(two=0; 2*two<=n; two++){
     one = n - 2 * two;
     printf("%d %d\n", one, two);
-    answer += fac1(one, two) % 1234567;
-    printf("%d\n", tem);
+    tem = (fac1(one, two) / fac2(one, two)) % 1234567;
+    answer += tem;
+    printf("%lld\n", tem);
   }
 
   answer %= 1234567;
@@ -40,8 +49,8 @@ long long solution(int n) {
 */
 int main(){
 
-  int n = 4;
-  printf("%lld\n", fac1(3, 4));
+  int n = 10;
+  fac2(100, 5);
   //solution(n);
   return 0;
 }
